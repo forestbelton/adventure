baea4e923f179f94ab5004dec786102925313604
Forest Belton <forest@homolo.gy>
2018-03-04 15:23:20 -0800

    Pad health and mana

--- src/component/HeroStats.js
+++ src/component/HeroStats.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const leftPad = (str, len, ch = '\u00a0')=> {
+    const paddingCount = Math.max(len - str.toString().length, 0)
+    let padding = ''
+
+    for (let i = 0; i < paddingCount; ++i) {
+        padding += ch
+    }
+
+    return padding + str
+}
+
 const UnstyledHeroStats = ({ className, hero }) => (
     <div className={className}>
         <div>Name:&nbsp;&nbsp;{hero.name}</div>
         <div>Class: {hero.job}</div>
         <br />
-        <div>HP: {hero.health.current} / {hero.health.maximum}</div>
-        <div>MP: {hero.mana.current} / {hero.mana.maximum}</div>
+        <div>HP: {leftPad(hero.health.current, 3)} / {leftPad(hero.health.maximum, 3)}</div>
+        <div>MP: {leftPad(hero.mana.current, 3)} / {leftPad(hero.mana.maximum, 3)}</div>
         <br />
         <div>STR: {hero.stats.strength}</div>
         <div>INT: {hero.stats.intelligence}</div>
