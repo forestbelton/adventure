3ce030fcc24df54df0ebbefc1de7c303c072bafb
Forest Belton <forest@homolo.gy>
2018-03-04 11:18:54 -0800

    Add a few more classes

--- src/game/data/Classes.js
+++ src/game/data/Classes.js
@@ -9,6 +9,26 @@ const Classes = {
             dexterity: [2, 3]
         }
     },
+    'Wizard': {
+        description: "Will it be a wingardium leviosa or an avada kedavra? You don't want to stick around to find out.",
+        stats: {
+            strength: [1, 1],
+            intelligence: [3, 7],
+            vitality: [1, 2],
+            luck: [1, 1],
+            dexterity: [2, 3]
+        }
+    },
+    'Acrobat': {
+        description: 'Adept in moving nimbly and jumping around.',
+        stats: {
+            strength: [2, 3],
+            intelligence: [1, 2],
+            vitality: [2, 2],
+            luck: [1, 2],
+            dexterity: [3, 6]
+        }
+    },
     'Cat': {
         description: 'Cats are relatively weak, but their agility and stroke of luck make up for it.',
         stats: {
